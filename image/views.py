6066c0a1068f753898333eae2e7b9d233ba14919
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-10-02 11:56:12 +0100

    tag image

--- image/views.py
+++ image/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from image.serializers import ImageSerializer
 from image.models import Image
@@ -7,3 +9,11 @@ from image.models import Image
 class ImageView(viewsets.ReadOnlyModelViewSet):
     serializer_class = ImageSerializer
     queryset = Image.objects.all()
+
+    @action(detail=True, methods=['patch'], name='Tag image')
+    def tag_image(self, request, pk=None):
+        """Tag an image"""
+        image = self.get_object()
+        image.tagged = True
+        image.save()
+        return Response({'status': 'Image tagged'})
